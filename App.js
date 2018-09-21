65bfab7e767100bb3b2ec19b477fa8e2b5fcbbd2
Isgi Arriza <isgiarriza@gmail.com>
2018-09-22 01:43:18 +0700

    init component

--- App.js
+++ App.js
@@ -5,7 +5,15 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.header}>
+          <Text style={styles.title}>Todo App</Text>
+        </View>
+        <View style={styles.content}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+        <View style={styles.footer}>
+
+        </View>
       </View>
     );
   }
@@ -14,8 +22,28 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff'
+  },
+  header: {
+    flex: 1,
+    backgroundColor: '#aaa',
+    justifyContent: 'center',
+    paddingTop: 20,
+    paddingLeft: 20
+  },
+  content: {
+    flex: 7,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  footer: {
+    flex: 1,
+    backgroundColor: '#aaa'
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20
+  }
 });
